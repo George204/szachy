ab60f9a57d36f0dd30ff78462db2d5bb8475d856
George204 <hubert.lazowy@gmail.com>
2023-03-05 00:08:13 +0100

    piony

--- main.py
+++ main.py
@@ -1,59 +1,81 @@
 import pygame, sys
+
 print(pygame.__version__)
 res = (800, 800)
 pygame.init()
 screen = pygame.display.set_mode(res, pygame.RESIZABLE)
-rbiałych = True
 
 clock = pygame.time.Clock()
 board = pygame.image.load("z\\board.png").convert()
 board = pygame.transform.scale(board, res)
 bp = pygame.image.load("z\\bp.png").convert_alpha()
-bp = pygame.transform.scale(bp, (res[0]/8, res[1]/8))
+bp = pygame.transform.scale(bp, (res[0] / 8, res[1] / 8))
 br = pygame.image.load("z\\br.png").convert_alpha()
-br = pygame.transform.scale(br, (res[0]/8, res[1]/8))
+br = pygame.transform.scale(br, (res[0] / 8, res[1] / 8))
 bn = pygame.image.load("z\\bn.png").convert_alpha()
-bn = pygame.transform.scale(bn, (res[0]/8, res[1]/8))
+bn = pygame.transform.scale(bn, (res[0] / 8, res[1] / 8))
 bb = pygame.image.load("z\\bb.png").convert_alpha()
-bb = pygame.transform.scale(bb, (res[0]/8, res[1]/8))
+bb = pygame.transform.scale(bb, (res[0] / 8, res[1] / 8))
 bq = pygame.image.load("z\\bq.png").convert_alpha()
-bq = pygame.transform.scale(bq, (res[0]/8, res[1]/8))
+bq = pygame.transform.scale(bq, (res[0] / 8, res[1] / 8))
 bk = pygame.image.load("z\\bk.png").convert_alpha()
-bk = pygame.transform.scale(bk, (res[0]/8, res[1]/8))
+bk = pygame.transform.scale(bk, (res[0] / 8, res[1] / 8))
 wp = pygame.image.load("z\\wp.png").convert_alpha()
-wp = pygame.transform.scale(wp, (res[0]/8, res[1]/8))
+wp = pygame.transform.scale(wp, (res[0] / 8, res[1] / 8))
 wr = pygame.image.load("z\\wr.png").convert_alpha()
-wr = pygame.transform.scale(wr, (res[0]/8, res[1]/8))
+wr = pygame.transform.scale(wr, (res[0] / 8, res[1] / 8))
 wn = pygame.image.load("z\\wn.png").convert_alpha()
-wn = pygame.transform.scale(wn, (res[0]/8, res[1]/8))
+wn = pygame.transform.scale(wn, (res[0] / 8, res[1] / 8))
 wb = pygame.image.load("z\\wb.png").convert_alpha()
-wb = pygame.transform.scale(wb, (res[0]/8, res[1]/8))
+wb = pygame.transform.scale(wb, (res[0] / 8, res[1] / 8))
 wq = pygame.image.load("z\\wq.png").convert_alpha()
-wq = pygame.transform.scale(wq, (res[0]/8, res[1]/8))
+wq = pygame.transform.scale(wq, (res[0] / 8, res[1] / 8))
 wk = pygame.image.load("z\\wk.png").convert_alpha()
-wk = pygame.transform.scale(wk, (res[0]/8, res[1]/8))
+wk = pygame.transform.scale(wk, (res[0] / 8, res[1] / 8))
 pygame.display.set_caption("Szachy")
 programIcon = pygame.image.load("z\\bk.png").convert_alpha()
 pygame.display.set_icon(programIcon)
 a, b = 0, 0
 wrence = False
+ruch = "w"
+ruchy = []
+def moz(x,p):
+    mozruchy = []
+    if x[2] == "p":
+        if x[1] == "w":
+            if (p[0]-1, p[1]) not in startpos:
+                mozruchy.append((p[0]-1, p[1]))
+            if p[0] == 7:
+                if (p[0]-2, p[1]) not in startpos:
+                    mozruchy.append((p[0]-2, p[1]))
+            if (p[0]-1, p[1]-1) in startpos:
+                mozruchy.append(((p[0]-1,p[1]-1)))
+            if (p[0]-1, p[1]+1) in startpos:
+                mozruchy.append(((p[0]-1,p[1]+1)))
+        if x[1] == "b":
+            if (p[0]+1, p[1]) not in startpos:
+                mozruchy.append((p[0]+1, p[1]))
+            if p[0] == 2:
+                if (p[0]+2, p[1]) not in startpos:
+                    mozruchy.append((p[0]+2, p[1]))
+            if (p[0]+1, p[1]-1) in startpos:
+                mozruchy.append(((p[0]+1,p[1]-1)))
+            if (p[0]+1, p[1]+1) in startpos:
+                mozruchy.append(((p[0]+1,p[1]+1)))
+    return(mozruchy)
 
-def xval(posval):
-    x = ((posval % 10) - 1) * res[1]/8
-    return x
 
-
-def yval(posval):
-    y = (posval // 10 - 1) * res[1]/8
-    return y
 
 
 startpos = {
-
-    11: br, 12: bn, 13: bb, 14: bq, 15: bk, 16: bb,  17: bn, 18: br,
-    21: bp, 22: bp, 23: bp, 24: bp, 25: bp, 26: bp,  27: bp, 28: bp,
-    81: wr, 82: wn, 83: wb, 84: wq, 86: wb, 85: wk, 87: wn, 88: wr,
-    71: wp, 72: wp, 73: wp, 74: wp, 76: wp, 75: wp, 77: wp, 78: wp,
+    (1, 1): (br, "b", "r"), (1, 2): (bn, "b", "n"), (1, 3): (bb, "b", "b"), (1, 4): (bq, "b", "q"),
+    (1, 5): (bk, "b", "k"), (1, 6): (bb, "b", "b"), (1, 7): (bn, "b", "n"), (1, 8): (br, "b", "r"),
+    (2, 1): (bp, "b", "p"), (2, 2): (bp, "b", "p"), (2, 3): (bp, "b", "p"), (2, 4): (bp, "b", "p"),
+    (2, 5): (bp, "b", "p"), (2, 6): (bp, "b", "p"), (2, 7): (bp, "b", "p"), (2, 8): (bp, "b", "p"),
+    (8, 1): (wr, "w", "r"), (8, 2): (wn, "w", "n"), (8, 3): (wb, "w", "b"), (8, 4): (wq, "w", "q"),
+    (8, 6): (wb, "w", "b"), (8, 5): (wk, "w", "k"), (8, 7): (wn, "w", "n"), (8, 8): (wr, "w", "r"),
+    (7, 1): (wp, "w", "p"), (7, 2): (wp, "w", "p"), (7, 3): (wp, "w", "p"), (7, 4): (wp, "w", "p"),
+    (7, 6): (wp, "w", "p"), (7, 5): (wp, "w", "p"), (7, 7): (wp, "w", "p"), (7, 8): (wp, "w", "p"),
 }
 while True:
     for event in pygame.event.get():
@@ -65,33 +87,44 @@ while True:
             fake_screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            (a, b) = pos
-            a, b = (a // (res[1]/8)+1), (b // (res[1]/8)+1)*10
-            clickval = a+b
+            clickval = ((pos[1] // (res[1] / 8) + 1), pos[0] // (res[1] / 8) + 1)
 
             if not wrence:
                 if clickval in startpos:
-                    screen.get_at(pos)[:3]
-                    print(screen.get_at(pos)[:1])
-                    fig_wrence = startpos[clickval]
-                    del startpos[clickval]
-                    wrence = True
+                    if startpos[clickval][1] == ruch:
+                        fig_wrence = startpos[clickval]
+                        del startpos[clickval]
+                        wrence = True
+                        ruchy = moz(fig_wrence, clickval)
+                        poczontek = clickval
             elif wrence:
-                startpos[clickval] = fig_wrence
-                wrence = False
+                if clickval == poczontek:
+                    startpos[clickval] = fig_wrence
+                    wrence = False
+                elif clickval in ruchy:
+                    startpos[clickval] = fig_wrence
+                    wrence = False
+                    if ruch == "w":
+                        ruch = "b"
+                    else:
+                        ruch = "w"
+
+
 
     # Ticking
     dt = clock.tick()
 
     # render
-
+    # screen.fill((0,0,0))
     screen.blit(board, (0, 0))
     for i in startpos:
-        screen.blit(startpos[i], (xval(i), yval(i)))
-
+        screen.blit(startpos[i][0], ((i[1] - 1) * res[1] / 8, (i[0] - 1) * res[1] / 8,))
     if wrence:
-        x = pygame.mouse.get_pos()[1]-res[1]/16
-        y = pygame.mouse.get_pos()[0]-res[1]/16
-        screen.blit(fig_wrence, (y, x))
+        for i in ruchy:
+            pygame.draw.circle(screen,radius=res[1]/16,center=(((i[1] - 1) * res[1] / 8)+res[1]/16, ((i[0] - 1) * res[1] / 8)+res[1]/16), color=(0,0,0),width=2)
+    if wrence:
+        x = pygame.mouse.get_pos()[1] - res[1] / 16
+        y = pygame.mouse.get_pos()[0] - res[1] / 16
+        screen.blit(fig_wrence[0], (y, x))
 
     pygame.display.flip()
